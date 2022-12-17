8fe8470fc3fb871959dc5749f691de5af5d0b24f
ShubhamTiwari479 <67504568+ShubhamTiwari479@users.noreply.github.com>
2022-12-18 01:28:26 +0530

    complete setup of web app

---
+++ lib/app/utils/deleteimage.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:take/globar_variables/globals.dart' as globals;
+
+void deletefuntion() {
+  var updatedlist = globals.imageList;
+}
---
+++ lib/web/utils/deleteimage.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:take/app/globar_variables/globals.dart' as globals;
+
+void deletefuntion() {
+  var updatedlist = globals.imageList;
+}
